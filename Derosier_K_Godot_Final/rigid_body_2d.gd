397c5ea94e2fcf7854023150f2eee20b625e0f25
KalebDerosier <144828782+KalebDerosier@users.noreply.github.com>
2023-12-15 09:14:58 -0800

    Add files via upload

---
+++ Derosier_K_Godot_Final/rigid_body_2d.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var sprite_frame = ["walk", "swim", "fly"]
+	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
+	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
